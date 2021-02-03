1ed302c268086e04bfd0cd8030d3d1c3c486cad4
Upd4ting <upd4ting@gmail.com>
2021-02-03 17:11:46 +0100

    feat: implement the basic hook stuff

---
+++ src/before-prepare.js
@@ -0,0 +1,10 @@
+const fs = require("fs-extra");
+const AndroidManifest = require("androidmanifest");
+const iOSPList = require("plist");
+
+module.exports = function (
+  $logger,
+  $platformsDataService,
+  $projectData,
+  hookArgs
+) {};
