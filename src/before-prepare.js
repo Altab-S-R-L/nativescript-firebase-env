95293a6a4da9eee617eaa198276820618f64236a
Upd4ting <upd4ting@gmail.com>
2021-02-04 12:35:39 +0100

    feat: implement base behaviour

--- src/before-prepare.js
+++ src/before-prepare.js
@@ -1,10 +1,47 @@
 const fs = require("fs-extra");
-const AndroidManifest = require("androidmanifest");
-const iOSPList = require("plist");
+const path = require("path");
 
 module.exports = function (
   $logger,
   $platformsDataService,
   $projectData,
   hookArgs
-) {};
+) {
+  const platform = hookArgs.prepareData.platform;
+  const appDir = $projectData.projectDir;
+  const extension = hookArgs.prepareData.env.production ? ".prod" : ".dev";
+
+  if (platform === "android") {
+    const file = `google-services.json${extension}`;
+    const filePath = path.join(appDir, file);
+
+    if (fs.existsSync(filePath)) {
+      fs.copyFileSync(
+        filePath,
+        path.join(
+          appDir,
+          "app",
+          "App_Resources",
+          "Android",
+          "google-services.json"
+        )
+      );
+    }
+  } else if (platform === "ios") {
+    const file = `GoogleService-Info.plist${extension}`;
+    const filePath = path.join(appDir, file);
+
+    if (fs.existsSync(filePath)) {
+      fs.copyFileSync(
+        filePath,
+        path.join(
+          appDir,
+          "app",
+          "App_Resources",
+          "iOS",
+          "GoogleService-Info.plist"
+        )
+      );
+    }
+  }
+};
